782c37e0f6c6f85e388881c00f4e64593d1b25c9
Hamed Jabbari <jabbary.hamed@gmail.com>
2021-03-11 23:23:25 -0800

    Gameover component

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { createStage } from '../gameHelpers';
+
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris'
 
+import { usePlayer } from '../hooks/usePlayer'
+import { useStage } from '../hooks/useStage'
 
 import Stage from './Stage';
 import Display from './Display';
@@ -10,16 +12,28 @@ import StartButton from './StartButton'
 
 
 const Tetris = () => {  
+
+    const [ dropTime, setDropTime ] = useState(null);
+    const [ gameOver, setGameover ] = useState(false);
+ 
+    const [player] = usePlayer();
+    const [stage, setStage] = useStage(player);
+
+    console.log('re-render');
     return(
         <StyledTetrisWrapper>
             <StyledTetris>
-            <Stage stage={createStage()}/> 
+            <Stage stage={stage}/> 
             <aside>
+                {gameOver ? (
+                    <Display gameOver={gameOver} text="game over " />
+                ) : (
                 <div>
                     <Display text="Score" />
                     <Display text="Rows" />
                     <Display text="Level" />
                 </div>
+                 )}
                 <StartButton />
             </aside>
             </StyledTetris>
